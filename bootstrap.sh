024f8c286021ff8ad6e252f97bb385fa7c1dfddd
akorn <devnull@localhost>
2011-02-24 18:38:25 +0200

    Added bitbucket.org hostfingerprints

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,14 @@ apt-get -yqq dist-upgrade
 # Install packages
 apt-get -yqq install mercurial htop && apt-get clean
 
+# Install bitbucket.org fingerprint
+cat <<EOF > ~/.hgrc
+[hostfingerprints]
+bitbucket.org = 81:2b:08:90:dc:d3:71:ee:e0:7c:b4:75:ce:9b:6c:48:94:56:a1:fe
+[ui]
+username = Firstname Lastname <user@email.com>
+EOF
+
 # Grab settings
 hg clone https://bitbucket.org/akorn/mindref-etc /usr/local/etc
 hg clone https://bitbucket.org/akorn/mindref-sbin /usr/local/sbin
