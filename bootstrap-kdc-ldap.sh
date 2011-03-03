407a2be8acf3cb9034458557d6b6eeaae8cff25e
akorn <devnull@localhost>
2011-03-03 12:04:15 +0200

    Disabled avahi-daemon, removed network-managed

--- bootstrap-kdc-ldap.sh
+++ bootstrap-kdc-ldap.sh
@@ -3,8 +3,9 @@
 hostname=`hostname -s`
 domain=`hostname -d`
 
-if [ ! -z $domain ]; then
-    domain=.$domain
+if [ -z $domain ]; then
+    echo "Unknown domain"
+    exit 1
 fi
 
 #cat <<EOF > /etc/hostname
@@ -18,14 +19,16 @@ EOF
 fi
 
 cat <<EOF > /etc/hosts
-127.0.0.1   localhost   $hostname$domain
+127.0.0.1   localhost
+127.0.0.1   $hostname.$domain   $hostname
 EOF
 
 echo net.ipv6.conf.all.disable_ipv6=1 >\
 $rootfs/etc/sysctl.d/disableipv6.conf
-exit 0
+
+update-rc.d avahi-daemon disable
 dhclient -v -r
-apt-get -y purge network-manager avahi-daemon
+apt-get -y purge network-manager
 
 hostname -F /etc/hostname
 dhclient -v
